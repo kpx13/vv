44abc02c49b6bcfc5550d71f1edade438eba83e0
Ann Kpx <annkpx@gmail.com>
2014-05-08 18:17:39 +0400

    ya templ

--- vv/views.py
+++ vv/views.py
@@ -207,11 +207,15 @@ def yandex(request):
         Transaction(message=str(request.POST.dict())).save()
         return HttpResponse(status=200)
     else:
+        c = get_common_context(request)
+        c['title'] = u'Оплата доступа на конференцию'
+        return render_to_response('yandex.html', c, context_instance=RequestContext(request))
         #raise Http404()
+        """
         import urllib2
         import urllib
         post_data = [('operation_id','905738277376062017'),]   
         result = urllib2.urlopen('http://money.yandex.ru/api/operation-details', urllib.urlencode(post_data))
         content = result.read()
         return HttpResponse(content)
-    
+        """
