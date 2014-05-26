34089a331605764652d200587f2ef8fd93125b6c
Ann Kpx <annkpx@gmail.com>
2014-05-26 14:15:58 +0400

    upd

--- vv/settings.py
+++ vv/settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'vv',   # Or path to database file if using sqlite3.
         'USER': 'vv',                      # Not used with sqlite3.
-        'PASSWORD': 'vv',                  # Not used with sqlite3.
+        'PASSWORD': 'P7t1P2b4',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
